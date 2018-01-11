5792e2f6cd1a7f4d986be543805496044c85a051
Robert Anderson <robert@noisysocks.com>
2018-01-11 15:50:22 +1100

    Trigger typing mode when ENTER is pressed
    Splitting a block or inserting a new paragraph block by pressing ENTER
    should engage typing mode.

--- blocks/editable/index.js
+++ blocks/editable/index.js
@@ -570,7 +570,6 @@ export default class Editable extends Component {
 				if ( event.shiftKey || ! this.props.onSplit ) {
 					this.editor.execCommand( 'InsertLineBreak', false, event );
 				} else {
-					event.stopImmediatePropagation();
 					this.splitContent();
 				}
 			}
